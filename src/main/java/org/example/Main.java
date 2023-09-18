39b3e4f8b422308e3cd61ecbb2d524b7866716fc
깃chengsukim <kcs981123@naver.com>
2023-09-18 10:54:34 +0900

    3rd

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello and welcome!");
+        System.out.println("Hello java!");
 
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
